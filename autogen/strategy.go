6f6999e5ad5d25ddab1c71d6281600939540d29d
Zixuan Guo <guozixua@grinnell.edu>
2020-12-14 21:05:11 +0800

    add status code to yaml struct

--- autogen/strategy.go
+++ autogen/strategy.go
@@ -2,6 +2,7 @@ package autogen
 
 import (
 	"fmt"
+	"github.com/hsluoyz/modsecurity-go/seclang/parser"
 	"strconv"
 
 	"github.com/waflab/waflab/autogen/operator"
@@ -19,6 +20,17 @@ func ProcessIndependentRule(ruleString string) (YAMLs []*test.Testfile) {
 		reversed, _ := operator.ReverseOperator(rule.Operator)
 		reversed = transformer.ReverseTransform(rule.Actions.Trans, reversed)
 
+		var statusCode int
+		for _, action := range rule.Actions.Action {
+			switch action.Tk {
+			case parser.TkActionAllow, parser.TkActionPass:
+				statusCode = 200
+			case parser.TkActionDeny:
+				statusCode = 403
+			default:
+			}
+		}
+
 		for i, variable := range rule.Variable {
 			if i >= len(v.Tests) {
 				v.Tests = append(v.Tests, &test.Test{
@@ -31,6 +43,7 @@ func ProcessIndependentRule(ruleString string) (YAMLs []*test.Testfile) {
 			}
 			v.Tests[i].TestTitle = fmt.Sprintf("%s-%s", strconv.Itoa(rule.Actions.Id), strconv.Itoa(i))
 			payload.AddVariable(variable, reversed, v.Tests[i].Stages[0].Stage.Input)
+			v.Tests[i].Stages[0].Stage.Output.Status = []int{statusCode}
 		}
 
 		YAMLs = append(YAMLs, v)
