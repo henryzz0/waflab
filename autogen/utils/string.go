02c7d731ea0c5e2301c810ee6e8b01329fd6eefb
Zixuan Guo <guozixua@grinnell.edu>
2020-12-27 16:29:55 +0800

    add utils func RandomStringFromSet

--- autogen/utils/string.go
+++ autogen/utils/string.go
@@ -6,11 +6,17 @@ import (
 
 const charSet string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
 
-// RandomString generate a random string with given length using character from charSet
-func RandomString(length int) string {
+// RandomStringFromSet generate a string by concating freq numbers of strings randomly draw from set.
+// Notice that string draw from set may be repetitive.
+func RandomStringFromSet(freq int, set []string) string {
 	var builder strings.Builder
-	for i := 0; i < length; i++ {
-		builder.WriteByte(charSet[randomGenerator.rand.Intn(len(charSet))])
+	for i := 0; i < freq; i++ {
+		builder.WriteString(set[randomGenerator.rand.Intn(len(set))])
 	}
 	return builder.String()
+}
+
+// RandomString generate a random string with given length using character from charSet.
+func RandomString(length int) string {
+	return RandomStringFromSet(length, strings.Split(charSet, ""))
 }
