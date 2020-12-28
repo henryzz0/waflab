4985107a139cca4102fa2fa8d81e0036eb7eaa2e
Zixuan Guo <guozixua@grinnell.edu>
2020-12-28 15:39:26 +0800

    hardcode charset to []string

--- autogen/utils/string.go
+++ autogen/utils/string.go
@@ -4,7 +4,9 @@ import (
 	"strings"
 )
 
-const charSet string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
+var charset = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+	"s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+	"O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_"}
 
 // RandomStringFromSet generate a string by concating freq numbers of strings randomly draw from set.
 // Notice that string draw from set may be repetitive.
@@ -18,7 +20,7 @@ func RandomStringFromSet(freq int, set []string) string {
 
 // RandomString generate a random string with given length using character from charSet.
 func RandomString(length int) string {
-	return RandomStringFromSet(length, strings.Split(charSet, ""))
+	return RandomStringFromSet(length, charset)
 }
 
 // PickRandomString picks a random string from the given string slice
