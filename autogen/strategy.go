fb8c4bda8f868a75dcd17c2d9aaaa63b09f4d26b
Zixuan Guo <guozixua@grinnell.edu>
2021-01-18 17:24:38 +0800

    refactor strategy; add wrapper to decide what kind of strategy to take (chained, independent, etc)

--- autogen/strategy.go
+++ autogen/strategy.go
@@ -2,6 +2,7 @@ package autogen
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 
 	"github.com/hsluoyz/modsecurity-go/seclang/parser"
@@ -14,56 +15,97 @@ import (
 	"github.com/waflab/waflab/test"
 )
 
-func ProcessIndependentRule(ruleString string) (YAMLs []*test.Testfile) {
-	rules := rule.ParseRuleDataToList(ruleString)
-	for _, rule := range rules {
-		v := yaml.DefaultYAML()
-
-		// set meta information
-		v.Meta.Author = "Microsoft Research Asia"
-		v.Meta.Name = fmt.Sprintf("%d.yaml", rule.Actions.Id)
-		v.Meta.Description = "This YAML file is automatically generated using AutoGen"
-
-		// reverse generate operator and transformation
-		reversed, _ := operator.ReverseOperator(rule.Operator)
-		reversed = transformer.ReverseTransform(rule.Actions.Trans, reversed)
-
-		// set status code
-		var statusCode int
-		for _, action := range rule.Actions.Action {
-			switch action.Tk {
-			case parser.TkActionAllow, parser.TkActionPass:
-				statusCode = 200
-			case parser.TkActionDeny:
-				statusCode = 403
-			default:
-			}
-		}
-
-		// process variable index exclusion
-		newVariables, err := processIndexExclusion(rule.Variable)
-		if err != nil {
-			panic(err)
-		}
-		rule.Variable = newVariables
-
-		for i, variable := range rule.Variable {
-			if i >= len(v.Tests) {
-				v.Tests = append(v.Tests, &test.Test{
-					Stages: []*test.StageWrapper{
-						{
-							Stage: yaml.DefaultStage(),
-						},
-					},
-				})
-			}
-			v.Tests[i].TestTitle = fmt.Sprintf("%s-%s", strconv.Itoa(rule.Actions.Id), strconv.Itoa(i+1))
-			payload.AddVariable(variable, reversed, v.Tests[i].Stages[0].Stage.Input)
-			v.Tests[i].Stages[0].Stage.Output.Status = []int{statusCode}
-		}
-
-		YAMLs = append(YAMLs, v)
+func GenerateTests(ruleString string) (YAMLs []*test.Testfile) {
+	rules, err := rule.ParseRuleDataToList(ruleString)
+	if err != nil {
+		log.Printf("Err while parsing rule string %s\n", ruleString)
+		return nil
 	}
 
+	for _, rule := range rules {
+		if rule.Actions.Chain { // Chained rule
+			log.Printf("Err chain rule %d not supported\n", rule.Actions.Id)
+			return nil
+		}
+		if t := processIndependentRule(rule); t != nil {
+			YAMLs = append(YAMLs, processIndependentRule(rule))
+		}
+	}
 	return YAMLs
+}
+
+func processIndependentRule(rule *parser.RuleDirective) *test.Testfile {
+	v := yaml.DefaultYAML()
+
+	// set meta information
+	v.Meta.Author = "Microsoft Research Asia"
+	v.Meta.Name = fmt.Sprintf("%d.yaml", rule.Actions.Id)
+	v.Meta.Description = "This YAML file is automatically generated using AutoGen"
+
+	// reverse generate operator and transformation
+	reversed, err := operator.ReverseOperator(rule.Operator)
+	if err != nil {
+		log.Printf("Rule %d: skip generation, %v\n", rule.Actions.Id, err)
+		return nil
+	}
+	reversed = transformer.ReverseTransform(rule.Actions.Trans, reversed)
+
+	// set status code
+	var statusCode int
+	for _, action := range rule.Actions.Action {
+		switch action.Tk {
+		case parser.TkActionAllow, parser.TkActionPass:
+			statusCode = 200
+		case parser.TkActionDeny, parser.TkActionBlock:
+			statusCode = 403
+		default:
+		}
+	}
+
+	// process variable index exclusion
+	newVariables, err := processIndexExclusion(rule.Variable)
+	if err != nil {
+		log.Printf("Rule %d: skip, %v", rule.Actions.Id, err)
+		return nil
+	}
+	rule.Variable = newVariables
+
+	current := 0 // number of test case generated
+	for _, variable := range rule.Variable {
+		// expand the tests slice if necessary
+		if current >= len(v.Tests) {
+			v.Tests = append(v.Tests, &test.Test{
+				Stages: []*test.StageWrapper{
+					{
+						Stage: yaml.DefaultStage(),
+					},
+				},
+			})
+		}
+		// add variable
+		err = payload.AddVariable(variable, reversed, v.Tests[current].Stages[0].Stage.Input)
+		if err != nil {
+			log.Printf("Rule %d: skip %s, %v\n",
+				rule.Actions.Id,
+				parser.VariableNameMap[variable.Tk],
+				err)
+			continue
+		}
+		// add title and status
+		v.Tests[current].TestTitle = fmt.Sprintf("%s-%s",
+			strconv.Itoa(rule.Actions.Id),
+			strconv.Itoa(current+1))
+		v.Tests[current].Stages[0].Stage.Output.Status = []int{statusCode}
+
+		current++
+	}
+
+	// it is possible that all variable is not supported in a rule,
+	// we therefore skip it entirely
+	if current == 0 {
+		log.Printf("Rule %d: no avaliable variable, skip entirely", rule.Actions.Id)
+		return nil
+	}
+
+	return v
 }
